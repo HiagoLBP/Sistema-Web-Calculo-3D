8650e3c9123bfbc88f3fadc7043e13cdcc569462
HiagoLBP <hiagogbisouza@gmail.com>
2026-08-26 16:03:01 -0300

    refactor: substituir parâmetros físicos da máquina pelo impressoraId no DTO

--- src/main/java/com/hsprints3d/model/CalculoRequest.java
+++ src/main/java/com/hsprints3d/model/CalculoRequest.java
@@ -1,18 +1,15 @@
 package com.hsprints3d.model;
 
+
 public record CalculoRequest(
         double pesoPeca,
         double precoRolo,
         double pesoRolo,
-        double potenciaWatts,
         double horasImpressao,
         double precoKwh,
-        double precoImpressora,
-        double vidaUtilHoras,
-        double manutencaoAno,
-        double horasUsoAno,
         double tempoTrabalhoMin,
         double valorHoraTrabalho,
         double markupDesejado,
-        double taxaMarketplace
+        double taxaMarketplace,
+        Long impressoraId
 ) {}
