41674616d78a4d84bb17a987bc9c67b753fd2b47
HiagoLBP <hiagogbisouza@gmail.com>
2026-09-03 20:33:24 -0300

    feat: CRUD de impressoras, layout mobile e Swagger

--- src/main/java/com/hsprints3d/controller/ImpressoraController.java
+++ src/main/java/com/hsprints3d/controller/ImpressoraController.java
@@ -2,6 +2,10 @@ package com.hsprints3d.controller;
 
 import com.hsprints3d.model.Impressora;
 import com.hsprints3d.repository.ImpressoraRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.config.RepositoryNameSpaceHandler;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +21,35 @@ public class ImpressoraController {
     }
 
     @PostMapping
-    public Impressora cadastrar(@RequestBody Impressora impressora){
+    public Impressora cadastrar(@Valid @RequestBody Impressora impressora){
         return repository.save(impressora);
     }
 
     @GetMapping
     public List<Impressora> listarTodas(){
         return repository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Impressora> atualizar(@PathVariable Long id, @Valid @RequestBody Impressora dados) {
+        return repository.findById(id).map(imp -> {
+            imp.setNome(dados.getNome());
+            imp.setPotenciaWatts(dados.getPotenciaWatts());
+            imp.setPrecoImpressora(dados.getPrecoImpressora());
+            imp.setManutencaoAno(dados.getManutencaoAno());
+            imp.setVidaUtilHoras(dados.getVidaUtilHoras());
+            imp.setHorasUsoAno(dados.getHorasUsoAno());
+
+            return ResponseEntity.ok(repository.save(imp));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        if (repository.existsById(id)){
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
